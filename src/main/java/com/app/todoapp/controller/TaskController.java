9f605d60eb427b3183d1f40809d8982367c12202
Vikrant Patel <90693378+VikrantP16@users.noreply.github.com>
2025-07-30 19:34:50 +0530

    Add form for creating new tasks in tasks.html

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.app.todoapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 // import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,6 @@ public class TaskController {
     @PostMapping
     public String createTask(@RequestParam String title) {
         taskService.createTask(title);
-        return "redirect:/";
+        return "redirect:/";    // creating redirection once a task has been added
     }
 }
